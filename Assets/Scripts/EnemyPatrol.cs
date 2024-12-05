9f41bad8284465dbd59c55e891fe1e8bf7d4ed2a
Enekoitz59 <enekoitzirujo0@gmail.com>
2024-12-04 17:32:33 -0800

    added enemy partrol scripts and created a flyer enemy

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+    public GameObject pointA;
+    public GameObject pointB;
+    public float speed;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        anim.SetBool("isFlying", true);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+
+        else
+        {
+            rb.velocity = new Vector2(-speed,0);
+        }
+
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+        }
+    }
+}
