43ff72ac561ec6780e3442e6364f3a70b248d9f4
Enekoitz59 <enekoitzirujo0@gmail.com>
2024-12-05 16:46:28 -0800

    Fixed Flyer AI

--- Assets/Scripts/EnemyScripts/EnemyChase.cs
+++ Assets/Scripts/EnemyScripts/EnemyChase.cs
@@ -8,6 +8,7 @@ public class EnemyChase : MonoBehaviour
 
     public GameObject player;
     public float speed;
+    public float distToChase;
 
 
     private float distance;
@@ -30,7 +31,7 @@ public class EnemyChase : MonoBehaviour
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 180;
 
 
-        if(distance < 4)
+        if(distance < distToChase)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(Vector3.forward * angle);
