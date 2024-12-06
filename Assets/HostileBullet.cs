5a521b671188d15dd5c06b8e78c3da3d6832cd91
manuelh177 <manuelhernandez@unr.edu>
2024-12-05 22:30:15 -0800

    Shooting Enemy

--- Assets/HostileBullet.cs
+++ Assets/HostileBullet.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class HostileBullet : MonoBehaviour
 {
+    private Vector3 mousePos;
+    private Rigidbody2D rb;
+    private Transform playerPos;
+    public float force;
+
+
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+        Transform curPos = GetComponent<Transform>();
+        float direction = playerPos.position.x - curPos.position.x;
+        rb.velocity = new Vector2(direction, 0).normalized * force;
     }
 
-    // Update is called once per frame
-    void Update()
+    /*private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.collider.CompareTag("Player"))
+        {
+            var health = collision.collider.GetComponent<Health>();
+            if(health != null)
+            {
+                health.TakeDamage(1);
+            }
+        }
+    }*/
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            var health = collision.GetComponent<Health>();
+            if (health != null)
+            {
+                health.TakeDamage(1);
+            }
+        }
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/EnemyScripts/ZyrkoWithGunScript.cs
+++ Assets/Scripts/EnemyScripts/ZyrkoWithGunScript.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class ZyrkoWithGunScript : MonoBehaviour
 {
+    private Camera mainCam;
+    private Transform transf;
+    public GameObject bullet;
+    private GameObject player;
+    private Transform playerPos;
+    public Transform gun;
+    private float timer;
+    public float timeBetweenShots;
+    private bool isFacingLeft;
+    private bool playerIsToTheLeft;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerPos = player.GetComponent<Transform>();
+        mainCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        transf = GetComponent<Transform>();
+        timer = timeBetweenShots * 0.85f;
+        isFacingLeft = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerIsToTheLeft = playerPos.position.x < transf.position.x;
+        if(isFacingLeft != playerIsToTheLeft)
+        {
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+            isFacingLeft = !isFacingLeft;
+        }
+        float enemyInCam = mainCam.WorldToViewportPoint(transf.position).x;
+        if (0 <= enemyInCam && enemyInCam <= 1)
+        {
+            if(timer > timeBetweenShots)
+            {
+                Shoot();
+                timer = 0;
+            }
+            timer += Time.deltaTime;
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bullet, gun.position, Quaternion.identity);
     }
 }
