7aa1e45aa60e11c9f5726ecfae468ae3b83ace43
Pablo Macias <123843648+Pablo-Macias@users.noreply.github.com>
2024-12-12 08:43:11 -0800

    Push
    Push for new version

---
+++ Assets/Scripts/ZyrkoScript.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZyrkoScript : MonoBehaviour
+{
+    private Health zyrko;
+    private SpriteRenderer sp;
+    public float flashDuration;
+    private Transform transf;
+    //public Health player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        zyrko = GetComponent<Health>();
+        sp = GetComponent<SpriteRenderer>();
+        transf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        if (collision.CompareTag("Projectile"))
+        {
+            zyrko.TakeDamage(1);
+            StartCoroutine(DamageFlash());
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Player"))
+        {
+            var healthComponent = collision.collider.GetComponent<Health>();
+            if(healthComponent != null)
+            {
+                healthComponent.TakeDamage(1);
+            }
+            StartCoroutine(PlayerKnockback(collision));
+        }
+    }
+
+    private IEnumerator DamageFlash()
+    {
+            sp.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(flashDuration);
+            sp.color = new Color(1, 1, 1, 1);
+            yield return new WaitForSeconds(flashDuration);
+    }
+
+    private IEnumerator PlayerKnockback(Collision2D collision)
+    {
+        collision.collider.GetComponent<PlayerMovement>().enabled = false;
+        if (transf.position.x > collision.collider.transform.position.x)
+        {
+            collision.collider.attachedRigidbody.velocity = new Vector2(-1f, 1f);
+        }
+        else
+        {
+            collision.collider.attachedRigidbody.velocity = new Vector2(1f, 1f);
+        }
+        yield return new WaitForSeconds(0.2f);
+        collision.collider.GetComponent<PlayerMovement>().enabled = true;
+    }
+}
